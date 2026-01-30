6d3c79b8be430a29bfe9c58638ad7583eb4bae6b
saqhibbilal <saqhibbilal@gmail.com>
2026-01-30 17:32:05 +0530

    quantico and theme

--- frontend/src/components/SupplyMap.jsx
+++ frontend/src/components/SupplyMap.jsx
@@ -92,8 +92,8 @@ export default function SupplyMap({ nodes = [], edges = [] }) {
         scrollWheelZoom={true}
       >
         <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://carto.com/attributions">CARTO</a>'
+          url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
         />
         {hasData && <FitBounds nodes={nodes} />}
         <NodeMarkers nodes={nodes} />
